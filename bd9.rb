ea3c92a7d0e66722ca3ee88de8997769b395e2b6
Steve Scammon <stevescammon@me.com>
2023-03-31 14:44:48 -0400

    homebrew

---
+++ bd9.rb
@@ -0,0 +1,15 @@
+class bd9 < Formula
+  desc "GitHub Commit Backdater"
+  homepage "https://github.com/scamelot/bd9"
+  url "https://github.com/scamelot/bd9/releases/download/v1.0.0/bd9.zip"
+  sha256 "c588d7eab3da61381fae76d1f2028be233a70282444c962fd71fd5a85a949655"
+  version "1.0.0"
+
+  def install
+    bin.install "bd9"
+  end
+
+  test do
+    system "#{bin}/bd9", "-h"
+  end
+end
