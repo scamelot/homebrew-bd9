9c886fc516c0acfcddca2590dace8c5fc4b9592a
Steve Scammon <stevescammon@me.com>
2023-04-01 13:11:05 -0400

    new build test new v

--- bd9.rb
+++ bd9.rb
@@ -1,7 +1,7 @@
 class Bd9 < Formula
   desc "GitHub Commit Backdater"
   homepage "https://github.com/scamelot/bd9"
-  url "https://github.com/scamelot/bd9/releases/download/v1.0.1/bd9-macos.zip"
+  url "https://github.com/scamelot/bd9/releases/download/v1.0.2/bd9-macos.zip"
   sha256 "02ff74ddae0c0d87586c2612710dbadcb75d1115415341b64025a899c19435e4"
   version "1.0.2"
 
